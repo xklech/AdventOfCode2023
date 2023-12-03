aa36d12bbb06396729d61c9573ca75138484c983
Jaroslav Klech <jaroslav.klech@threatmark.com>
2023-12-03 15:56:51 +0100

    Day 3 solution

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,9 +1,145 @@
+/**
+ * --- Day 3: Gear Ratios ---
+
+You and the Elf eventually reach a gondola lift station; he says the gondola lift will take you up to the water source, but this is as far as he can bring you. You go inside.
+
+It doesn't take long to find the gondolas, but there seems to be a problem: they're not moving.
+
+"Aaah!"
+
+You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. "Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; it'll still be a while before I can fix it." You offer to help.
+
+The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+
+The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+
+Here is an example engine schematic:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+
+Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+
+Your puzzle answer was 551094.
+ * 
+ */
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const TRACE = false;
+
+const REGEX = /\d+/g;
+
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let result = 0;
+  for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
+    const line = data[rowIndex];
+    let match: RegExpExecArray;
+    while ((match = REGEX.exec(line)) != null) {
+      let partLines = new PartLines();
+      partLines.current = line;
+      partLines.next = rowIndex + 1 < data.length ? data[rowIndex + 1] : null;
+      partLines.previous = rowIndex - 1 >= 0 ? data[rowIndex - 1] : null;
+      if (TRACE)
+        console.log(
+          `Number: ${match[0]}, match index: ${match.index}, rowIndex: ${rowIndex} `
+        );
+
+      if (isValidPart(partLines, match[0], match.index)) {
+        if (TRACE) console.log(`Valid part: ${match[0]}`);
+        result += +match[0];
+      }
+    }
+  }
+
+  return result;
+}
+
+function isValidPart(
+  partLines: PartLines,
+  partNumber: string,
+  startIndex: number
+): boolean {
+  return (
+    isCurrentLineValidPart(partLines.current, partNumber, startIndex) ||
+    isLineValidPart(partLines.previous, partNumber, startIndex) ||
+    isLineValidPart(partLines.next, partNumber, startIndex)
+  );
+}
+
+function isCurrentLineValidPart(
+  currentLine: string,
+  partNumber: string,
+  startIndex: number
+): boolean {
+  let prevIndex = startIndex - 1;
+  let oneAfterIndex = startIndex + partNumber.length;
+  if (TRACE) console.log(`Length: ${currentLine.length}, Line: ${currentLine}`);
+  if (TRACE)
+    console.log(
+      `Check current line: prevIndex: ${prevIndex}, oneAfterIndex: ${oneAfterIndex}}`
+    );
+  if (prevIndex >= 0 && currentLine.charAt(prevIndex) != '.') {
+    if (TRACE) console.log(`Valid current line part - prev`);
+    return true;
+  }
+  if (
+    oneAfterIndex < currentLine.length &&
+    currentLine.charAt(oneAfterIndex) != '.'
+  ) {
+    if (TRACE) console.log(`Valid current line part - after`);
+    return true;
+  }
+  if (TRACE) console.log(`Not valid current line part`);
+  return false;
+}
+
+function isLineValidPart(
+  line: string,
+  partNumber: string,
+  startIndex: number
+): boolean {
+  if (line == null) {
+    return false;
+  }
+  let start = startIndex - 1 < 0 ? 0 : startIndex - 1;
+  let end =
+    startIndex + partNumber.length > line.length
+      ? startIndex + partNumber.length
+      : startIndex + partNumber.length + 1;
+  if (TRACE) console.log(`Line: ${line}`);
+  if (TRACE) console.log(`Check line: start: ${start}, end: ${end}}`);
+  const search = line.substring(start, end);
+  for (const char of [...search]) {
+    if (char != '.') {
+      if (TRACE) console.log(`Valid line part`);
+      return true;
+    }
+  }
+
+  if (TRACE) console.log(`Not valid line part`);
+  return false;
+}
+
+class PartLines {
+  current: string = null;
+  previous: string = null;
+  next: string = null;
+
+  public toString(): string {
+    return `Current: ${this.current}\n Previous: ${this.previous}\n Next: ${this.next}`;
+  }
 }
 
 const answer = await day3a();
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,9 +1,166 @@
+/**
+ * --- Part Two ---
+
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
+What is the sum of all of the gear ratios in your engine schematic?
+
+Your puzzle answer was 80179647.
+
+ * 
+ */
+
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const TRACE = false;
+
+const REGEX = /\*/g;
+
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let result = 0;
+  for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
+    const line = data[rowIndex];
+    let match: RegExpExecArray;
+    while ((match = REGEX.exec(line)) != null) {
+      if (TRACE) console.log('===================');
+      let partLines = new PartLines(data, rowIndex);
+
+      if (TRACE)
+        console.log(`Match index: ${match.index}, rowIndex: ${rowIndex} `);
+      let temp: number[] = [];
+      temp = temp
+        .concat(getCurrentLineAdjacentNumbers(partLines.current, match.index))
+        .concat(getLineAdjacentNumbers(partLines.previous, match.index))
+        .concat(getLineAdjacentNumbers(partLines.next, match.index));
+      if (TRACE)
+        console.log(
+          chalk.bgBlueBright('Found numbers in vicinity: ') + ' ' + temp
+        );
+      if (temp.length == 2) {
+        if (TRACE)
+          console.log(
+            chalk.bgCyan('Exactly two numbers found. Adding to result.')
+          );
+        result += temp[0] * temp[1];
+      }
+    }
+  }
+
+  return result;
+}
+
+function getCurrentLineAdjacentNumbers(
+  currentLine: string,
+  starIndex: number
+): number[] {
+  let prevIndex = starIndex - 1;
+  let oneAfterIndex = starIndex + 1;
+
+  if (TRACE) console.log(`Line: ${currentLine}`);
+
+  let result: number[] = [];
+
+  if (prevIndex >= 0 && isNumeric(currentLine.charAt(prevIndex))) {
+    let number = expandNumber(currentLine, prevIndex);
+    result.push(number);
+  }
+  if (
+    oneAfterIndex < currentLine.length &&
+    isNumeric(currentLine.charAt(oneAfterIndex))
+  ) {
+    let number = expandNumber(currentLine, oneAfterIndex);
+    result.push(number);
+  }
+  if (TRACE) console.log(`Numbers before after: ${result}`);
+  return result;
+}
+
+function getLineAdjacentNumbers(line: string, starIndex: number): number[] {
+  if (line == null) {
+    return [];
+  }
+  let result = new Set<number>();
+  let start = starIndex - 1 < 0 ? 0 : starIndex - 1;
+  let end = starIndex + 1 > line.length ? starIndex + 1 : starIndex + 1 + 1;
+  if (TRACE) console.log(`Line: ${line}`);
+  if (TRACE) console.log(`Check line: start: ${start}, end: ${end}`);
+  const search = line.substring(start, end);
+  for (let charIndex = 0; charIndex < search.length; charIndex++) {
+    const char = search[charIndex];
+    if (isNumeric(char)) {
+      let number = expandNumber(line, start + charIndex);
+      if (TRACE) console.log(`Digit found. Expand. Number ${number}`);
+      result.add(number);
+    }
+  }
+  if (TRACE) console.log(`Returning numbers:  ${[...result]}`);
+  return [...result];
+}
+
+function expandNumber(line: string, index: number): number {
+  let result = line[index];
+  let indexLeft = index;
+  while (true) {
+    if (indexLeft-- > 0 && isNumeric(line[indexLeft])) {
+      result = line[indexLeft] + result;
+    } else {
+      break;
+    }
+  }
+  let indexRight = index;
+  while (true) {
+    if (indexRight++ < line.length - 1 && isNumeric(line[indexRight])) {
+      result = result + line[indexRight];
+    } else {
+      break;
+    }
+  }
+  return +result;
+}
+
+function isNumeric(str: string): boolean {
+  return /^\d$/.test(str);
+}
+
+class PartLines {
+  current: string = null;
+  previous: string = null;
+  next: string = null;
+
+  constructor(data: readonly string[], rowIndex: number) {
+    this.current = data[rowIndex];
+    this.next = rowIndex + 1 < data.length ? data[rowIndex + 1] : null;
+    this.previous = rowIndex - 1 >= 0 ? data[rowIndex - 1] : null;
+  }
+
+  public toString(): string {
+    return `Current: ${this.current}\n Previous: ${this.previous}\n Next: ${this.next}`;
+  }
 }
 
 const answer = await day3b();
