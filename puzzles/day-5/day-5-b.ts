30765845b29ad543626f2245ccd12c6a6e58dc0a
Jaroslav Klech <jaroslav.klech@threatmark.com>
2023-12-06 10:39:21 +0100

    DAY 5 b, disclaimer added

--- puzzles/day-5/day-5-b.ts
+++ puzzles/day-5/day-5-b.ts
@@ -16,6 +16,14 @@ In the above example, the lowest location number can be obtained from seed numbe
 
 Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
 
+
+Your puzzle answer was 136096660.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+==================================================================
+== REWRITTEN TO .NET AND BRUTE FORCED ON 20 THREADS, 12 minutes ==
+==================================================================
  * 
  */
 
@@ -31,19 +39,21 @@ export async function day5b(dataPath?: string) {
   const data = await readData(dataPath);
   let relations = parseRelations(data);
 
-  let locations: number[] = [];
-  
+  let min = Number.MAX_VALUE;
   let seeds = relations.seeds;
   for (let i = 0; i < seeds.length; i = i + 2) {
     let startSeed = seeds[i];
-    let lastSeed = seeds[i] + seeds[i+1];
+    let lastSeed = seeds[i] + seeds[i + 1];
     for (let seed = startSeed; seed < lastSeed; seed++) {
-      locations.push(relations.resolveLocation(seed));
+      let location = relations.resolveLocation(seed);
+      if (location < min) {
+        min = location;
+        console.log('new min: ' + min);
+      }
     }
   }
 
-  log(`Locations: ${locations}`);
-  return Math.min(...locations);
+  return min;
 }
 
 function parseRelations(data: string[]): Relations {
@@ -51,34 +61,33 @@ function parseRelations(data: string[]): Relations {
   let iterator = data[Symbol.iterator]();
 
   let maps: Map<string, CleverMap> = new Map([
-    ["seed-to-soil", relations.seedsToSoil],
-    ["soil-to-fertilizer", relations.soilToFertilizer],
-    ["fertilizer-to-water", relations.fertilizerToWater],
-    ["water-to-light", relations.waterToLight],
-    ["light-to-temperature", relations.lightToTemperature],
-    ["temperature-to-humidity", relations.temperatureToHumidity],
-    ["humidity-to-location", relations.humidityToLocation]
+    ['seed-to-soil', relations.seedsToSoil],
+    ['soil-to-fertilizer', relations.soilToFertilizer],
+    ['fertilizer-to-water', relations.fertilizerToWater],
+    ['water-to-light', relations.waterToLight],
+    ['light-to-temperature', relations.lightToTemperature],
+    ['temperature-to-humidity', relations.temperatureToHumidity],
+    ['humidity-to-location', relations.humidityToLocation],
   ]);
 
   let iterResult: IteratorResult<string, any>;
-  while(!(iterResult = iterator.next()).done) {
+  while (!(iterResult = iterator.next()).done) {
     let line: string = iterResult.value;
     log(`Line: ${line}`);
-    if(line === "") {
-      log("Empty line");
+    if (line === '') {
+      log('Empty line');
       continue;
     }
     let sectionKey = (line.match(REGEX) as RegExpMatchArray)[0];
-    log(chalk.bgCyan(`Section: ${sectionKey}`))
-    if (sectionKey === "seeds") {
+    log(chalk.bgCyan(`Section: ${sectionKey}`));
+    if (sectionKey === 'seeds') {
       let match = line.match(REGEX_NUMBER);
-      relations.seeds = match ? match.map(str => +str) : [];
+      relations.seeds = match ? match.map((str) => +str) : [];
       log(`Seeds: ${relations.seeds}`);
     } else {
       let map = maps.get(sectionKey) as CleverMap;
       consumeMap(map, iterator);
-      log(chalk.bgMagenta("Map: ") + map.toString());
-      
+      log(chalk.bgMagenta('Map: ') + map.toString());
     }
   }
   return relations;
@@ -86,21 +95,21 @@ function parseRelations(data: string[]): Relations {
 
 function consumeMap(cleverMap: CleverMap, iterator: IterableIterator<string>) {
   let iterResult: IteratorResult<string, any>;
-  while((iterResult = iterator.next())) {
+  while ((iterResult = iterator.next())) {
     let line = iterResult.value;
-    if(iterResult.done || line == "") {
+    if (iterResult.done || line == '') {
       return;
     }
-    log("Consume Line: " + line);
+    log('Consume Line: ' + line);
     let match = line.match(REGEX_NUMBER);
-    if(match) {
+    if (match) {
       cleverMap.add(new MapEntry(+match[0], +match[1], +match[2]));
     }
   }
 }
 
 class Relations {
-  seeds: number[] = []
+  seeds: number[] = [];
   seedsToSoil: CleverMap = new CleverMap();
   soilToFertilizer: CleverMap = new CleverMap();
   fertilizerToWater: CleverMap = new CleverMap();
@@ -111,58 +120,61 @@ class Relations {
 
   resolveLocation(seed: number): number {
     let transitions = [
-      this.seedsToSoil, 
-      this.soilToFertilizer, 
-      this.fertilizerToWater, 
-      this.waterToLight, 
-      this.lightToTemperature, 
-      this.temperatureToHumidity, 
-      this.humidityToLocation
-    ]
+      this.seedsToSoil,
+      this.soilToFertilizer,
+      this.fertilizerToWater,
+      this.waterToLight,
+      this.lightToTemperature,
+      this.temperatureToHumidity,
+      this.humidityToLocation,
+    ];
 
-      let step = seed;
-      for (const transition of transitions) {
-        step = transition.getRelation(step);
-      }
+    let step = seed;
+    for (const transition of transitions) {
+      step = transition.getRelation(step);
+    }
 
-      log(chalk.bgRedBright(`Seed ${seed} resolved to ${step}.`))
-      return step;
+    log(chalk.bgRedBright(`Seed ${seed} resolved to ${step}.`));
+    return step;
   }
 }
 
 class CleverMap {
-  map: MapEntry[]= [];
+  map: MapEntry[] = [];
 
   public add(entry: MapEntry) {
     this.map.push(entry);
   }
 
-  public getRelation(source: number): number{
-
-    log("Get realtion for source: " + source);
-    log("Map: " + this.map)
+  public getRelation(source: number): number {
+    log('Get realtion for source: ' + source);
+    log('Map: ' + this.map);
     for (const mapEntry of this.map) {
-      if(mapEntry.sourceStart <= source && mapEntry.sourceStart + mapEntry.length >= source) {
-        let resolution = (mapEntry.destionationStart + source - mapEntry.sourceStart);
-        log("Relation found, Dest: " + resolution);
+      if (
+        mapEntry.sourceStart <= source &&
+        mapEntry.sourceStart + mapEntry.length >= source
+      ) {
+        let resolution =
+          mapEntry.destionationStart + source - mapEntry.sourceStart;
+        log('Relation found, Dest: ' + resolution);
         return resolution;
       }
     }
-    log ("Relation not found, returning source: " + source);
+    log('Relation not found, returning source: ' + source);
     return source;
   }
 
   public toString(): string {
     return `CleverMap: ${this.map}\n`;
   }
-} 
+}
 
-class MapEntry{
+class MapEntry {
   sourceStart: number;
   destionationStart: number;
   length: number;
 
-  constructor (destStart: number, sourceStart: number, length: number) {
+  constructor(destStart: number, sourceStart: number, length: number) {
     this.destionationStart = destStart;
     this.sourceStart = sourceStart;
     this.length = length;
